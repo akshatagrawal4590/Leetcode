521c72837e4e48ce61944f30978717db09f8d166
akshatagrawal4590 <100948962+akshatagrawal4590@users.noreply.github.com>
2022-08-21 18:26:31 +0530

    Time: 180 ms (66.47%), Space: 93.4 MB (52.40%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+        int lsf = INT_MAX;
+        int op = 0;
+        int pist = 0;
+        
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < lsf){
+                lsf = prices[i];
+            }
+            pist = prices[i] - lsf;
+            if(op < pist){
+                op = pist;
+            }
+        }
+        return op;
+    }
+};
