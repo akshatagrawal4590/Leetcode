dd6a19852ba238890f6a54148e75b8374871ca27
akshatagrawal4590 <100948962+akshatagrawal4590@users.noreply.github.com>
2023-05-12 13:17:52 +0530

    Added solution - LeetHub

---
+++ Find length of Loop - GFG/find-length-of-loop.cpp
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+int countNodesinLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        cout<< countNodesinLoop(head) << endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+//Function to find the length of a loop in the linked list.
+int countNodesinLoop(struct Node *head)
+{
+    Node* temp = head;
+    if(head == NULL)
+    {
+        return 0;
+    }
+    while(temp != NULL)
+    {
+        temp -> data = 1e7;
+        if(temp -> next != NULL)
+        {
+            if(temp -> next -> data == 1e7)
+            {
+                int count = 0;
+                temp = temp -> next;
+                while(temp -> data == 1e7)
+                {
+                    count++;
+                    temp -> data = 1e8;
+                    temp = temp -> next;
+                }
+                return count;
+            }
+            temp = temp -> next;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
