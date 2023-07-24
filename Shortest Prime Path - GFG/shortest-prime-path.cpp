4378b6a95d4a9654386eba797095b8cc44a57cff
akshatagrawal4590 <100948962+akshatagrawal4590@users.noreply.github.com>
2023-07-24 18:37:03 +0530

    Added solution - LeetHub

--- Shortest Prime Path - GFG/shortest-prime-path.cpp
+++ Shortest Prime Path - GFG/shortest-prime-path.cpp
@@ -6,83 +6,78 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 class Solution{   
-  public:
-    void isprime(vector<int>&v){
-      
-       for(int i=2;i*i<10000;i++){
-           if(v[i]==1){
-               for(int j=i*i;j<10000;j+=i){
-                   v[j]=0;
-               }
-           }
+public:
+     int solve(int num1,int num2)
+   {   
+     //code here
+      int n =10000;
+        vector<bool> isPrime(n,1);
+isPrime[0]= false;
+isPrime[1]=false;
+for(int i=2;i<n;i++){
+   if(isPrime[i]==true){
+       for(int j=2*i;j<n;j+=i){
+           isPrime[j]=false;
        }
-      
    }
-    int shortestPath(int num1,int num2)
-    {   
-        // Complete this function using prime vector
-        vector<int>v(10000,1);
-        isprime(v);
-        
-        queue<pair<int,int>>q;
-        unordered_map<int,int>mp;
-        q.push({num1,0});
-         mp[num1]++;
-        while(!q.empty())
-        {
-            
-            int n=q.size();
-            while(n--)
-            {
-            
-            auto it=q.front();
-            q.pop();
-            int l=it.second;
-            int num=it.first;
-           
-         
-            if(num==num2) return l;
-            string s=to_string(num);
-            for(int i=0;i<4;i++)
-            {
-                string t=s;
-                for(int j=0;j<=9;j++)
-                {
-                    if(j==0 && i==0) continue;
-                    else
-                    {
-                        t[i]=(j+'0');
-                       
-                        int x=stoi(t);
-                        
-                        if(mp[x]==0 && x!=num && v[x])
-                        {
-                            mp[x]++;
-                            q.push({x,l+1});
-                        }
-                    }
-                }
-            }
+}
+if(num1==num2) return 0;
+vector<int> vis(n,0);
+queue<int> q;
+q.push(num1);
+vis[num1]=1;
+int count=0;
+while(!q.empty()){
+   int n = q.size();
+   for(int itr=0;itr<n;itr++){
+       int temp=q.front();
+       q.pop();
+   for(int i=0;i<4;i++){
+       for(int j=0;j<10;j++){
+           if(j==0 && i==0) continue;
 
-            }
-            
-        }
-        return -1;
-    }
+ 
+
+          //Converting number into string
+           string str= to_string(temp);
+
+ 
+
+         //Converting int to char
+           str[i] = j+ '0';
+
+ 
+
+         //Converting string to int
+           int num = stoi(str);
+
+
+           if(num==num2) return count+1;
+           if(vis[num] || !isPrime[num]) continue;
+           vis[num]=1;
+           q.push(num);
+       }
+   }  
+   }
+   count++;
+}
+return count;
+   }
 };
+
 
 //{ Driver Code Starts.
 signed main()
 {
-    int t;
-    cin>>t;
-    Solution obj;
-    while(t--)
-    {
-        int Num1,Num2;
-        cin>>Num1>>Num2;
-        int answer=obj.shortestPath(Num1,Num2);
-        cout<<answer<<endl;
-    }
+  int t;
+  cin>>t;
+  while(t--)
+  {
+      int Num1,Num2;
+      cin>>Num1>>Num2;
+      Solution obj;
+      int answer=obj.solve(Num1,Num2);
+      cout<<answer<<endl;
+  }
 }
 // } Driver Code Ends
